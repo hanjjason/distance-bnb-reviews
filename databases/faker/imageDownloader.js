17ab4411c0b0469e42bbe77afe801ad00fbdc496
Jason Han <han.j.jason@gmail.com>
2020-05-01 19:28:55 -0700

    seeding done

---
+++ databases/faker/imageDownloader.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const request = require('request');
+const path = require('path');
+
+var downloadImage = (url, filename) => {
+  request(url, {encoding: 'binary'}, function(error, response, body) {
+    fs.writeFile('./images/' + filename, body, 'binary', function (err) {});
+  });
+}
+
+var dlimages = (num) => {
+  for (var i = 0; i < num; i++) {
+    downloadImage('https://loremflickr.com/225/225/person', String(i) + '.jpg');
+  }
+}
+
+dlimages(150);
